558f90e100386b1c14ac324524574de7285c9a2e
Ger0r0r <fedor.bombax@gmial.com>
2023-04-08 23:56:07 +0300

    add comments

--- Arduino_ESP8266/main/server_handler.ino
+++ Arduino_ESP8266/main/server_handler.ino
@@ -16,11 +16,22 @@ void server_read(WiFiClient * client, char * command){
 }
 
 void server_parcer(WiFiClient * client, char * command, int * status, int * temp, int * level, int * ntemp){
+
+  char first[4] = command.substring(0,3);
+  // RECIEVE
   // "info" - send information
   // "stop" - break heat, stay wait
-  // "heat" - to hit water over MAXTEMP(default=90)
+  // "heat" - to hit water over MAX_HEAT_TEMP(default=90)
   // "keep XX" - keep XX tempreture (heat if it needed)
-  if (command == "info"){ // send information
+
+  // SEND
+  // *all information* - by recieving "info"
+  // "ok" - if task is accepted
+  // "done" - if task is done
+  // "Not enouth water" - water level is too low
+  // "Too much water" - water level is too high
+  // "already" - if current task is the same as recieved task
+  if (first == "info"){ // send information
     client.print("tempreture ");
     client.println(temp);
     client.print("water "); 
@@ -30,17 +41,30 @@ void server_parcer(WiFiClient * client, char * command, int * status, int * temp
     client.print("aim ");
     client.println(ntemp);
   }
-  if (command == "stop"){
+  if (first == "stop"){
     status = 0;
     client.println("ok");
+    client.println("done");
   }
-  if (command == "heat"){
+  if (first == "heat"){
     if (status == 1){
       client.println("already");
     }
     if ((status == 0) || (status == 2)){
-      status = 1;
-      client.pritnln("ok");
+      if (temp <= MAX_HEAT_TEMP){
+        if ((level >= WATER_LOW_BORDER) && (level <= WATER_HIGH_BORDER)){
+          status = 1;
+          client.pritnln("ok");
+        } else {
+          if (level < WATER_LOW_BORDER){
+            client.println("Not enouth water");
+          } else {
+            client.println("Too much water");
+          }
+        }
+      } else {
+        client.println("done")
+      }
     }
   }
   if (){
